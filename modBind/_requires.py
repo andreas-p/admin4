3061e94c98cb966c26df73508b10817d7168c77c
andreas-p <andreas-p@users.noreply.github.com>
2022-04-06 19:48:43 +0200

    relax requirement

--- modBind/_requires.py
+++ modBind/_requires.py
@@ -22,7 +22,7 @@
 def GetPrerequisites(info=False):
   try:
     import dns.version
-    if dns.version.version < "2.2.":
+    if dns.version.version < "2.0.":
       if info:
         print ("dnspython too old")
       return None
