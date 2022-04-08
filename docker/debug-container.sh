06b30175643f15e8b9974e250575850f96e48550
Andreas Pflug <dev@admin4.org>
2022-04-08 19:52:31 +0200

    debug-container.sh

---
+++ docker/debug-container.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+#
+# The Admin4 Project
+# (c) 2013-2022 Andreas Pflug
+#
+# Licensed under the Apache License,
+# see LICENSE.TXT for conditions of usage
+
+if [ -z "$(xhost | grep LOCAL:)" ] ; then
+  # allow local access
+  xhost local:root
+fi
+
+# defaults to local image
+image=${1-admin4}
+shift
+
+IFS=":" read user _pwd uid gid _name home _shell  <<< $(grep ^$(whoami): /etc/passwd)
+
+docker run -ti --rm --name admin4 $* \
+	--net=host --env=DISPLAY=unix$DISPLAY \
+	--volume /tmp/.X11-unix:/tmp/.X11-unix \
+	--volume /etc/passwd:/etc/passwd \
+        --volume $PWD/..:/src \
+        --volume $home:$home \
+	--user $uid:$gid \
+	$image /bin/bash
+
